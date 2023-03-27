4ae5ec76d791678d4feeb4a94f68102edbbedfae
Ravi <ravidudhatra@gmail.com>
2023-03-27 23:51:51 +0530

    Program file cleanup

--- SwastiFashionHub.WebApi/Program.cs
+++ SwastiFashionHub.WebApi/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using SwastiFashionHub.Core.Services.Interface;
 using SwastiFashionHub.Core.Services;
 using SwastiFashionHub.Data.Context;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
 using AutoMapper;
 using SwastiFashionHub.Core.Mappings;
 
@@ -28,15 +26,11 @@ builder.Services.AddDbContext<SwastiFashionHubLlpContext>(options =>
     }));
 
 
-//var config = new MapperConfiguration(cfg =>
-//{
-//    cfg.AddProfile(new DesignMapper());
-//});
 
 var config = new MapperConfiguration(c => {
     c.AddProfile<DesignMapper>();
-    
 });
+
 builder.Services.AddSingleton<IMapper>(s => config.CreateMapper());
 
 //builder.Services.AddAutoMapper(typeof(Program));
